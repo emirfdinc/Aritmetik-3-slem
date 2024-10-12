1ec9b10b6da2fa26ac83d716c349667b5a72a0b9
Emir Fatih <emir.fatih.dnc@gmail.com>
2024-10-12 16:08:14 +0300

    Add project files.

---
+++ Aritmetik 3 İslem/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Aritmetik_3_İslem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Hesapla = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(49, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(49, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayı2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(49, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sayı3:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(196, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 43);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(196, 192);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 43);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(196, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 43);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(255, 48);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sayi Giriniz";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(56, 256);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(866, 164);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Hesapla
+            // 
+            this.Hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Hesapla.Location = new System.Drawing.Point(348, 86);
+            this.Hesapla.Name = "Hesapla";
+            this.Hesapla.Size = new System.Drawing.Size(140, 43);
+            this.Hesapla.TabIndex = 8;
+            this.Hesapla.Text = "Topla";
+            this.Hesapla.UseVisualStyleBackColor = true;
+            this.Hesapla.Click += new System.EventHandler(this.Hesapla_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(603, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 193);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(348, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 46);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Çıkar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(348, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 46);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Çarp";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Hesapla);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hesapla";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Hesapla;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Aritmetik 3 İslem/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aritmetik_3_İslem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Hesapla_Click(object sender, EventArgs e)
+        {
+            Int32 sayi1,sayi2,sayi3,toplam;
+
+            sayi1 =Convert.ToInt32(textBox1.Text);
+            sayi2 =Convert.ToInt32(textBox2.Text);
+            sayi3 =Convert.ToInt32(textBox3.Text);
+            toplam=sayi1+sayi2+sayi3;
+            listBox1.Items.Add("Toplam Sonucu:" + "" + toplam);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int32 sayi1, sayi2, sayi3, cıkarma;
+            sayi1=Convert.ToInt32(textBox1.Text);
+            sayi2=Convert.ToInt32(textBox2.Text);
+            sayi3=Convert.ToInt32(textBox3.Text);
+            cıkarma = sayi1 - sayi2 - sayi3;
+            listBox1.Items.Add("Çıkarma Sonucu:" + cıkarma);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Int32 sayi1,sayi2,sayi3,carpma;
+
+            sayi1=Convert.ToInt32(textBox1.Text);
+            sayi2= Convert.ToInt32(textBox2.Text);
+            sayi3=Convert.ToInt32(textBox3.Text);
+            carpma = sayi1 * sayi2 * sayi3;
+            listBox1.Items.Add("Çarpma Sonucu:" +  carpma);
+                
+
+
+        }
+    }
+}
